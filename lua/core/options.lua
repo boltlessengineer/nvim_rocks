a14ea2f5142552976470b41b435d8ac422838e55
Seongmin Lee <boltlessengineer@gmail.com>
2024-08-18 13:41:00 +0000

    feat(options): `'formatexpr'` to `conform`

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -38,6 +38,7 @@ vim.o.foldcolumn = "0"
 vim.o.foldenable = true
 vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
+vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
 vim.o.formatoptions = "jcrqlnt"
 if vim.fn.executable("rg") ~= 0 then
     vim.o.grepprg = "rg --vimgrep"

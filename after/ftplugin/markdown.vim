572b1181ab0b8b2e38c2730a07ca9a9e4b5f1d50
Seongmin Lee <boltlessengineer@gmail.com>
2024-12-29 08:10:02 +0000

    chore: set keymap from `ftplugin` file

--- after/ftplugin/markdown.vim
+++ after/ftplugin/markdown.vim
@@ -2,3 +2,5 @@ setlocal wrap
 setlocal colorcolumn=
 setlocal tabstop=2
 setlocal shiftwidth=2
+
+nnoremap <buffer> <cr> <cmd>Markview toggle<cr>
--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -1,9 +1,3 @@
 require("markview").setup({
     initial_state = false
 })
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = "markdown",
-    callback = function(ev)
-        vim.keymap.set("n", "<cr>", "<cmd>Markview toggle<cr>", { buffer = ev.buf })
-    end
-})

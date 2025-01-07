f1b0cca7b889c32595087035b1ef7753cfa77795
Seongmin Lee <boltlessengineer@gmail.com>
2025-01-07 05:24:40 +0000

    feat: disable indent-blankline on markview enabled

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -1,3 +1,14 @@
 require("markview").setup({
-    initial_state = false
+    initial_state = false,
+    callbacks = {
+        -- FIX: this is buggy. maybe because it is called *after* enabling
+        on_enable = function(_buf, _win)
+            vim.cmd("IBLDisable")
+        end,
+        on_disable = function(_buf, _win)
+            vim.schedule(function()
+                vim.cmd("IBLEnable")
+            end)
+        end,
+    }
 })

0143b00cb4f50006cbb7b734a4fca2ce0220e820
Seongmin Lee <boltlessengineer@gmail.com>
2025-04-20 13:52:37 +0000

    fix: don't reverse `@markup.raw.block` hl

--- lua/core/highlights.lua
+++ lua/core/highlights.lua
@@ -19,6 +19,7 @@ local tbl = {
     ["github_dark_default"] = function()
         util_hl().set("StatusLineNC", { reverse = true, inherit = "StatusLine" })
         util_hl().set("@markup.raw", { italic = false, reverse = true })
+        util_hl().set("@markup.raw.block", { reverse = false, inherit = "@markup.raw" })
         util_hl().set("DiagnosticLineError", { bg = util_hl().tint(util_hl().get("DiagnosticVirtualTextError", "fg"), -0.8) })
         util_hl().set("DiagnosticLineWarn", { bg = util_hl().tint(util_hl().get("DiagnosticVirtualTextWarn", "fg"), -0.8) })
         util_hl().set("DiagnosticLineInfo", { bg = util_hl().tint(util_hl().get("DiagnosticVirtualTextInfo", "fg"), -0.8) })

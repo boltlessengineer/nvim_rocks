6b53d1519794d825e409bdd2eb10f369c151614d
BoltlessEngineer <boltlessengineer@gmail.com>
2025-05-14 17:30:50 +0900

    feat: use `birthtime` and `mtime` for `created` and `modified` fields

--- lua/snippets/norg.lua
+++ lua/snippets/norg.lua
@@ -10,8 +10,27 @@ local k = require("luasnip.nodes.key_indexer").new_key
 local rep = require("luasnip.extras").rep
 local fmt = require("luasnip.extras.fmt").fmt
 
+local DATE_FORMAT = "!%Y-%m-%dT%H:%M:%S+09:00"
+
 local function today_fn()
-    return os.date("!%Y-%m-%dT%H:%M:%S+00:00")
+    return os.date(DATE_FORMAT)
+end
+
+-- TODO: reimplement these with git
+local function btime_fn()
+    local path = vim.fn.expand("%")
+    local stat = vim.uv.fs_stat(path)
+    local btime = stat.birthtime or stat.mtime
+    btime = btime.sec
+    return os.date(DATE_FORMAT, btime)
+end
+
+local function mtime_fn()
+    local path = vim.fn.expand("%")
+    local stat = vim.uv.fs_stat(path)
+    local mtime = stat.mtime
+    mtime = mtime.sec
+    return os.date(DATE_FORMAT, mtime)
 end
 
 local function file_index_fn(_, snip)
@@ -23,6 +42,7 @@ local function file_index_fn(_, snip)
     else
         title = vim.fn.substitute(snip.env.TM_FILENAME, "\\..*$", "", "g")
     end
+    title = vim.fn.substitute(title, "-", " ", "g")
     return sn(nil, { i(1, title) })
 end
 
@@ -39,8 +59,8 @@ ls.add_snippets("norg", {
         ]],
         {
             d(1, file_index_fn),
-            f(today_fn, {}, { key = "today" }),
-            rep(k"today"),
+            f(btime_fn),
+            f(mtime_fn),
             -- t"",
             -- t"",
         }

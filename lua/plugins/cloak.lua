d4efea4f27f4656b1a0133f5208dd4fdc6456305
Seongmin Lee <git@boltless.me>
2025-11-23 13:43:41 +0900

    fic(plugins/cloak): correct dotenv file pattern
    Signed-off-by: Seongmin Lee <git@boltless.me>

--- lua/plugins/cloak.lua
+++ lua/plugins/cloak.lua
@@ -2,4 +2,7 @@ require("cloak").setup({
     cloak_length = 6,
     -- Re-enable cloak when a matched buffer leaves the window.
     cloak_on_leave = true,
+    patterns = {
+        file_pattern = '^.env$'
+    },
 })
